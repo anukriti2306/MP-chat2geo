f94ac36987cd041635e350b9a358b1e99cee849a
anukriti_23 <anukriti2306@gmail.com>
2025-08-29 12:30:53 +0530

    changed up the ui to dark mode, made changes to button styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import Index from "./pages/Index";
 import { ChatPage } from "./pages/ChatPage";
 import { MapPage } from "./pages/MapPage";
 import NotFound from "./pages/NotFound";
+import './index.css';
+import './App.css';
 
 const queryClient = new QueryClient();
 
@@ -16,31 +18,36 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <AuthProvider>
+        {/* Toasts */}
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route 
-              path="/chat/:chatId?" 
-              element={
-                <ProtectedRoute>
-                  <ChatPage />
-                </ProtectedRoute>
-              } 
-            />
-            <Route 
-              path="/map" 
-              element={
-                <ProtectedRoute>
-                  <MapPage />
-                </ProtectedRoute>
-              } 
-            />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+
+        {/* Main app container with dark mode */}
+        <div className="min-h-screen bg-gray-900 text-white dark:bg-gray-900 dark:text-white">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route 
+                path="/chat/:chatId?" 
+                element={
+                  <ProtectedRoute>
+                    <ChatPage />
+                  </ProtectedRoute>
+                } 
+              />
+              <Route 
+                path="/map" 
+                element={
+                  <ProtectedRoute>
+                    <MapPage />
+                  </ProtectedRoute>
+                } 
+              />
+              {/* Catch-all route */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
       </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
